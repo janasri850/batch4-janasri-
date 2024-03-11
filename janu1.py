15296c44664284e7154a5db46acf402247a8e47a
janasri850 <162983099+janasri850@users.noreply.github.com>
2024-03-11 16:10:13 +0530

    Add files via upload

---
+++ janu1.py
@@ -0,0 +1,29 @@
+a=8
+b=9
+print(a,b)
+
+a=7,b=9#error
+prin(a,b)
+a,b+7,9
+print(a)
+print(b)
+a=7
+b=7
+a=b=7
+print(a,b)
+a=8,7,9.89,"hello"
+print(a)
+print(type(a))
+a,b=78 #error
+print(a,b)
+
+# ----> variables
+ --->variable is a name which is used to identify the value of the variable
+---> ?which is stored in in specific memeory location
+# rules to define variables
+#1.--> variable have to start with only alphabets,underscores
+#2.--> the char following the 1st char can be alphabets,numbers,underscores
+#3.--> no spaces are allowed
+#4.--> no special charecters  like @#%^&%&$%
+#5.--> case sensitive
+#6.-->keywords are not allowed as variables
