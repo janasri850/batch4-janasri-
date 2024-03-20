e33d84ae349337d366780391649a00ce56311e41
janasri850 <162983099+janasri850@users.noreply.github.com>
2024-03-20 16:04:17 +0530

    Add files via upload

---
+++ p[ython7.py
@@ -0,0 +1,169 @@
+#set1 = {'python', 'java', 'data science'}
+#set2 = {'ML','AI','R language','python'}
+#set3 = {'Data Analytics', 'Robotics', 'Deep Learning'}
+#c = 0
+#flag = 0
+#for val in range(3):
+#    for val in set1:
+#        if val in set2 or val in set3:
+#            print("joint")
+#    for val in set2:
+#        if val in set1 or val in set3:
+#            print("joint")
+#    for val in set1:
+#        if val in set2 or val in set1:
+#            print("joint")
+            
+# 3.)
+#list1 = ["m","na","i","ke"]
+#list2 = ["y", "me", "s", "lly"]
+#13 = []
+#for val in range(len(list1)):
+#    ans = list1[val]+list2[val]
+#    13.append(ans)
+# print(13)
+
+# i=0
+#while i<len(list1)
+#     13.append(list1[i]+list2[i])
+#     i+=1
+#print(13)     
+
+# 0/p --> ['my', 'name', 'is', 'kelly']
+
+#Hacker rank --> caesar cipher
+# ! Functions
+#? DEF
+#Function is a block of code which is used to perform a specific functionally
+#Function can be created using def keyword
+
+# ? Function has 3 parts
+#Function declaration
+#Function defanition
+#Function cell
+
+#def greet():
+#    print("welcome user!!")
+#greet()    
+#greet()    
+#greet()
+#greet()    
+#greet()    
+#greet()    
+#greet()    
+#greet() # function calling   
+
+
+
+# ! Eg:2
+#def adding():
+#    a = 8
+#    b = 6
+#    c = 5
+#    d =a+b+c
+#    print(d)
+#adding()    
+#adding()    
+#adding()    
+#adding()    
+#adding() #function calling
+
+
+# ! Eg:2
+# ? Function with parameter
+#def greeting(name):
+#    print("Welcome",name)
+
+#for val in range(3):
+#    username = input("Enter the name: ")
+#    greeting(username)
+
+# ! Eg:3
+#def profile(name, age,place):
+#    print(name, age, place)
+#profile("sid", 29, "che")    
+
+#1.)Write a python script to generate and print a dictionary that
+#contains a number (between 1 and n)in the form(x, x*x).
+#sample Dictionary( n = 5):
+#Expected output : {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
